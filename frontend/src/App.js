087456885dccb9360ff6f11da2eca74bb88a9feb
syahrul mubarok <mubaroksyahrul8132gmail.com>
2021-10-28 10:30:03 +0700

    "update"

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container">
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <h2>welcome</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
